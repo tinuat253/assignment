7c090992c9981bde7453710088b9899bd61da7fa
tinuat253 <51506084+tinuat253@users.noreply.github.com>
2019-06-25 18:17:05 +0530

    Add files via upload

---
+++ tree.js
@@ -0,0 +1,34 @@
+var employee = [
+  { id: 1, managerId: null },
+  { id: 2, managerId: 1 },
+  { id: 3, managerId: 2 },
+  { id: 4, managerId: 3 },
+  { id: 5, managerId: 4 },
+  { id: 6, managerId: 1 }
+  
+]
+
+
+var root = [];
+
+var map = {};
+
+var managers=employee.filter(emp => {
+  
+  if (emp.managerId==null) return root.push(emp);
+  
+  
+  let managerIndex = map[emp.managerId];
+  if (typeof managerIndex !== "number") {
+    managerIndex = employee.findIndex(no => no.id === emp.managerId);
+    map[emp.managerId] = managerIndex;
+  }
+  
+  if (!employee[managerIndex].assignees) {
+    return employee[managerIndex].assignees = [emp];
+  }
+  
+  employee[managerIndex].assignees.push(emp);
+});
+
+console.log(JSON.stringify(root,null,2));
